43cf5bbed5c962bdda579faf68f7bb5ed1269605
Sriragbadveti <badvetisrirag@gmail.com>
2025-10-14 15:46:39 +0530

    fg

--- src/Pages/TicketDisplay.jsx
+++ src/Pages/TicketDisplay.jsx
@@ -69,7 +69,7 @@ const ticketTiers = [
   {
     name: "Standard +3 Workshops Ticket",
     id: "tier-allinclusive",
-    href: "/soldout",
+    href: "/standard-plus-3",
     
     description: [
       "Full MEDCON access + 3 workshops",
@@ -89,7 +89,7 @@ const ticketTiers = [
   {
     name: "Standard +4 Workshops Ticket",
     id: "tier-standard-plus-4",
-    href: "/soldout",
+    href: "/standard-plus-4",
     
     description: [
       "Full MEDCON access + 4 workshops",
